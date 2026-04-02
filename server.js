b9f27a0d1f0fa5fd9c7eff4d5d717125c686e5d6
Kochpi <raphael.picoche1@gmail.com>
2026-04-02 15:47:43 +0200

    express set up

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
+
 const app = express();
-const port = 8000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log("Server app listening on port " + port);
