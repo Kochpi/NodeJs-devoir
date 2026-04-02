a37aae053bb75034cdcc2699dc97f12d67562626
Kochpi <raphael.picoche1@gmail.com>
2026-04-02 15:43:18 +0200

    test2

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,12 +1,4 @@
-const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        mongoose.connect(process.env.MONGO-URI, () => console.log
-        ("Mongo connecté))
-    }
-    catch {
 
-    }
-}
+
+
